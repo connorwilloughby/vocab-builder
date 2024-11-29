d0589b5d977691092dc80ab3a8d322b25525c92c
Connor Willoughby <cwilloughby311@gmail.com>
2024-11-29 19:27:40 +0000

    data: save progress

--- data/progress/test.py
+++
@@ -1,7 +0,0 @@
-from shared.engine import ProgressState
-
-progress = ProgressState()
-
-progress.update_progress('sal', True,)
-progress.update_progress('con', True,)
-progress.update_progress('son', False,)
