26f47df57c40a12b44d76bb80db721684988843f
Connor Willoughby <cwilloughby311@gmail.com>
2024-11-30 14:12:52 +0000

    test: add skeleton

---
+++ test.py
@@ -0,0 +1,7 @@
+from shared.engine import ProgressState
+
+progress = ProgressState()
+
+progress.update_progress('sal', True,)
+progress.update_progress('con', True,)
+progress.update_progress('son', False,)
